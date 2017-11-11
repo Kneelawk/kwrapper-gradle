20c486d2928aa74cfde8b0c68ba30dd96d103928
Kneelawk <jedidiah.pommert@gmail.com>
2017-11-10 18:12:46 -0800

    Begin on plugin

---
+++ src/main/java/com/kneelawk/kwrapper/KWrapperPlugin.java
@@ -0,0 +1,13 @@
+package com.kneelawk.kwrapper;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class KWrapperPlugin implements Plugin<Project> {
+
+	@Override
+	public void apply(Project arg0) {
+
+	}
+
+}
